26aa5b97020040134fbe4a716d3a2a5a8d04b916
Sworld <1628767411@qq.com>
2022-01-15 11:04:43 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
+</template>
+
+<style lang="stylus">
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
