b596d8928e6bfdcb5a10675c1f2c2908fa6cd3ea
Sworld <1628767411@qq.com>
2022-01-26 22:09:08 +0800

    使添加游戏界面的上部分按钮有了作用

--- src/background.ts
+++ src/background.ts
@@ -1,82 +1,113 @@
-'use strict'
+"use strict";
 
-import { app, protocol, BrowserWindow } from 'electron'
-import { ipcMain } from 'electron'
-import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
-import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer'
-const isDevelopment = process.env.NODE_ENV !== 'production'
+import { app, protocol, BrowserWindow, nativeImage } from "electron";
+import { ipcMain, dialog } from "electron";
+import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
+import installExtension, { VUEJS3_DEVTOOLS } from "electron-devtools-installer";
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
-  { scheme: 'app', privileges: { secure: true, standard: true } }
-])
+  { scheme: "app", privileges: { secure: true, standard: true } },
+]);
 
-async function createWindow () {
+async function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    autoHideMenuBar:true,
+    autoHideMenuBar: true,
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: true,//(process.env.ELECTRON_NODE_INTEGRATION as unknown) as boolean,
-      contextIsolation:false
-    }
-  })
+      nodeIntegration: true, //(process.env.ELECTRON_NODE_INTEGRATION as unknown) as boolean,
+      contextIsolation: false,
+    },
+  });
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
-    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
+    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string);
+    if (!process.env.IS_TEST) win.webContents.openDevTools();
   } else {
-    createProtocol('app')
+    createProtocol("app");
     // Load the index.html when not in development
-    win.loadURL('app://./index.html')
+    win.loadURL("app://./index.html");
   }
 }
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', async () => {
+app.on("ready", async () => {
   if (isDevelopment && !process.env.IS_TEST) {
     // Install Vue Devtools
     try {
-      await installExtension(VUEJS3_DEVTOOLS)
+      await installExtension(VUEJS3_DEVTOOLS);
     } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString())
+      console.error("Vue Devtools failed to install:", e.toString());
     }
   }
-  createWindow()
-})
+  createWindow();
+});
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
-  if (process.platform === 'win32') {
-    process.on('message', (data) => {
-      if (data === 'graceful-exit') {
-        app.quit()
+  if (process.platform === "win32") {
+    process.on("message", (data) => {
+      if (data === "graceful-exit") {
+        app.quit();
       }
-    })
+    });
   } else {
-    process.on('SIGTERM', () => {
-      app.quit()
-    })
+    process.on("SIGTERM", () => {
+      app.quit();
+    });
   }
 }
+
+ipcMain.on("choose_save_directory", async (Event, arg) => {
+  // 选择游戏存档目录
+  const path = dialog.showOpenDialog({
+    title: "请选择存档路径",
+    properties: ["openDirectory"],
+  });
+  Event.reply("choose_save_directory_reply", await path);
+});
+
+ipcMain.on("choose_executable_file", async (Event, arg) => {
+  // 选择游戏可执行文件
+  const path = dialog.showOpenDialog({
+    title: "选择游戏可执行文件",
+    properties: ["openFile"],
+    filters: [{ name: "可执行程序", extensions: ["exe", "bat", "cmd", "jar"] }],
+  });
+  Event.reply("choose_executable_file_reply", await path);
+});
+
+ipcMain.on("choose_game_icon", async (Event, arg) => {
+  // 选择游戏图标
+  const path = dialog.showOpenDialog({
+    title: "选择游戏图标",
+    properties: ["openFile"],
+    filters: [{ name: "可识别图片", extensions: ["jpg", "png", "ico"] }],
+  });
+  const icon = nativeImage.createFromPath((await path).filePaths[0])
+  if (icon == undefined){return}
+  Event.reply("choose_game_icon_reply", icon.toDataURL());
+});
--- src/components/AddGame/MainContent.vue
+++ src/components/AddGame/MainContent.vue
@@ -1,19 +1,17 @@
 <template>
 	<el-card class="game-info">
 		<div class="top-part">
-			<img
-				class="game-icon"
-				src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-			/>
+			<img class="game-icon" :src="game_icon_src" />
 		</div>
 		<div style="padding: 14px" class="input-container">
 			<div class="bottom">
-				<el-button type="text" class="button">选择游戏图标 </el-button>
-				<el-input v-model="game_name" placeholder="请输入游戏名（必须）">
-				</el-input>
+				<el-button @click="choose_game_icon()" type="text" class="button"
+					>选择游戏图标
+				</el-button>
+				<el-input v-model="game_name" placeholder="请输入游戏名（必须）" />
 				<el-input v-model="save_path" placeholder="请选择存档路径（必须）">
 					<template #append>
-						<el-button>
+						<el-button @click="choose_save_directory()">
 							<el-icon>
 								<document-add />
 							</el-icon>
@@ -22,7 +20,7 @@
 				</el-input>
 				<el-input v-model="game_path" placeholder="请选择游戏启动文件">
 					<template #append>
-						<el-button>
+						<el-button @click="choose_executable_file()">
 							<el-icon>
 								<document-add />
 							</el-icon>
@@ -35,18 +33,48 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onMounted } from "vue";
 import { DocumentAdd } from "@element-plus/icons-vue";
+const { ipcRenderer } = require("electron");
 
 export default defineComponent({
-	setup() {},
+	mounted() {
+		ipcRenderer.on("choose_save_directory_reply", (Event, arg) => {
+			// 选择游戏存档目录
+			this.save_path = arg.filePaths[0] ? arg.filePaths[0] : this.save_path;
+		});
+		ipcRenderer.on("choose_executable_file_reply", (Event, arg) => {
+			// 选择游戏存档目录
+			this.game_path = arg.filePaths[0] ? arg.filePaths[0] : this.game_path;
+		});
+		ipcRenderer.on("choose_game_icon_reply", (Event, arg) => {
+			// 选择游戏图标地址
+			this.game_icon_src = arg;
+		});
+	},
 	components: { DocumentAdd },
 	data() {
 		return {
-			game_name: "",
-			save_path: "",
-			game_path: "",
+			game_name: "", // 写入游戏名
+			save_path: "", // 选择游戏存档目录
+			game_path: "", // 选择游戏启动程序
+			game_icon_src:
+				"https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png",
 		};
+	},
+	methods: {
+		choose_save_directory() {
+			// 选择游戏存档目录
+			ipcRenderer.send("choose_save_directory");
+		},
+		choose_executable_file() {
+			// 选择游戏启动程序
+			ipcRenderer.send("choose_executable_file");
+		},
+		choose_game_icon() {
+			// 选择游戏图标地址
+			ipcRenderer.send("choose_game_icon");
+		},
 	},
 });
 </script>
