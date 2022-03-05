2571054f38ab80a53ed91c8dd7edfa86baa32d31
Sworld <1628767411@qq.com>
2022-03-05 21:22:23 +0800

    注释

--- src/background/saveManager.ts
+++ src/background/saveManager.ts
@@ -9,6 +9,11 @@ import { TimeLike } from "original-fs";
 // TODO:把同步操作改成异步的以优化性能
 // TODO:修改已有的信息
 
+/**
+ * 通过游戏名得到游戏存档集合
+ * @param game_name 游戏名
+ * @returns 当前游戏的存档集合
+ */
 export function get_game_saves_info(game_name: string) {
     let config = get_config();
     let game_save_path = config.games[game_name].save_path;
@@ -18,7 +23,12 @@ export function get_game_saves_info(game_name: string) {
     return saves;
 }
 
-function dump_game_saves_info(game_name: string, new_saves: Saves) {
+/**
+ * 将该游戏原有的存档集合替换成新的
+ * @param game_name 游戏名
+ * @param new_saves 修改后的存档集合信息
+ */
+function set_game_saves_info(game_name: string, new_saves: Saves) {
     let config = get_config();
     let game_save_path = config.games[game_name].save_path;
     fs.writeFileSync(
@@ -54,7 +64,7 @@ export function backup_save(
     let saves = get_game_saves_info(game_name);
     saves.saves.push(save_info);
 
-    dump_game_saves_info(game_name, saves);
+    set_game_saves_info(game_name, saves);
     compress_to_file(game_save_path, backup_path, date);
 }
 
@@ -71,6 +81,11 @@ export function apply_backup(game_name: string, date: TimeLike) {
     extract_to_folder(backup_path, game_save_path);
 }
 
+/**
+ * 通过必要信息，创建一个游戏的空的存档集合
+ * @param game_name 游戏名
+ * @param icon 游戏图标
+ */
 function create_save_folder(game_name: string, icon: string) {
     let config = get_config();
     let saves: Saves = {
@@ -86,6 +101,13 @@ function create_save_folder(game_name: string, icon: string) {
     );
 }
 
+/**
+ * 通过必要信息，创建一个游戏的备份库
+ * @param game_name 游戏名
+ * @param save_path 游戏存档所在位置
+ * @param icon 游戏图标
+ * @param game_path 游戏启动路径
+ */
 export function create_game_backup(
     game_name: string,
     save_path: string,
