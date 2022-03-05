ffda33976ef573c15b640e8fc8125f2471b473b7
Sworld <1628767411@qq.com>
2022-03-05 20:02:11 +0800

    修正了应用存档潜在的问题（未删除老存档）

--- src/background/archive.ts
+++ src/background/archive.ts
@@ -3,6 +3,12 @@ import unzipper from "unzipper";
 import fs from "fs";
 import path from "path";
 
+/**
+ * 把一个文件夹压缩成一个zip
+ * @param source_path 原始文件夹
+ * @param target_path 存档zip文件的路径
+ * @param file_name 目标文件名
+ */
 export function compress_to_file(
     source_path: string,
     target_path: string,
@@ -27,10 +33,31 @@ export function compress_to_file(
     archive.finalize();
 }
 
+function clear_folder_recursive(folder_path: string) {
+    let files = fs.readdirSync(folder_path);
+    files.forEach((file) => {
+        let cur_path = path.join(folder_path, file);
+        if (fs.statSync(cur_path).isDirectory()) {
+            clear_folder_recursive(cur_path);
+        } else {
+            fs.unlinkSync(cur_path);
+        }
+    });
+    fs.rmdirSync(folder_path);
+}
+
+
+/**
+ * 清空目标文件夹,然后把一个压缩文件内容解压到那
+ * @param source_file_path 压缩文件
+ * @param target_path 目标文件夹
+ */
 export function extract_to_folder(
     source_file_path: string,
     target_path: string
 ) {
+    clear_folder_recursive(target_path);
+    
     fs.createReadStream(source_file_path).pipe(
         unzipper.Extract({ path: target_path })
     );
