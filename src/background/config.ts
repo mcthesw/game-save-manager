069d8de52b6099960aa5798880c36c2912675344
Sworld <1628767411@qq.com>
2022-03-18 23:08:36 +0800

    版本号

--- src/background/config.ts
+++ src/background/config.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import { Config } from "./saveTypes";
 
 export let default_config: Config = {
-    version: "0.1.6",
+    version: "0.1.7",
     backup_path: "./save_data",
     games: {},
     settings:{
