3cda61ba6acfe1c990df1a0337c2e431ca7b8477
Sworld <t.sworld@qq.com>
2023-04-03 19:47:11 +0800

    complete compress_to_file

--- src-tauri/src/archive.rs
+++ src-tauri/src/archive.rs
@@ -1,16 +1,64 @@
 use std::{
     fs::{self, File},
-    io::{self, BufReader, Write},
-    os,
+    io::{self, BufReader, Read, Seek, Write},
     path::PathBuf,
 };
 
-use anyhow::{Error, Ok, Result};
+use anyhow::{Ok, Result};
 use zip::ZipWriter;
-use zip_extensions::write::ZipWriterExtensions;
 
 use crate::config::{SaveUnit, SaveUnitType};
 
+/// [Code reference](https://github.com/matzefriedrich/zip-extensions-rs/blob/master/src/write.rs#:~:text=%7D-,fn,create_from_directory_with_options,-\()
+/// 
+/// Write `origin` folder to zip `writer`, the files will in `prefix_path`
+/// 
+/// Normally, `prefix_path` should be the file name of the `origin` folder
+fn add_directory<T>(
+    writer: &mut ZipWriter<T>,
+    origin: &PathBuf,
+    prefix_path: &PathBuf,
+) -> Result<()>
+where
+    T: std::io::Write,
+    T: Seek,
+{
+    let mut paths = Vec::new();
+    paths.push(origin);
+
+    let mut buffer = Vec::new();
+
+    while let Some(next) = paths.pop() {
+        let directory_entry_iter = std::fs::read_dir(next)?;
+
+        for entry in directory_entry_iter {
+            let entry = entry?;
+            let entry_path = entry.path();
+            let entry_metadata = std::fs::metadata(&entry_path)?;
+            if entry_metadata.is_file() {
+                let mut f = File::open(&entry_path)?;
+                f.read_to_end(&mut buffer)?;
+                let mut cur_path = prefix_path.clone();
+                cur_path = cur_path.join(entry.file_name());
+
+                writer.start_file(
+                    cur_path.to_str().unwrap(),
+                    zip::write::FileOptions::default(),
+                )?;
+                writer.write_all(&buffer)?;
+                buffer.clear();
+            } else if entry_metadata.is_dir() {
+                let mut cur_path = prefix_path.clone();
+                cur_path = cur_path.join(&entry.file_name());
+                add_directory(writer, &entry_path, &cur_path)?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+/// Compress a set of save to a zip file in `backup_path` with name 'date.zip'
 pub fn compress_to_file(
     save_paths: Vec<SaveUnit>,
     backup_path: &PathBuf,
@@ -28,7 +76,8 @@ pub fn compress_to_file(
             }
             SaveUnitType::Folder => {
                 let path = PathBuf::from(&x.path);
-                zip.create_from_directory(&path)?;
+                let root = PathBuf::from(path.file_name().unwrap());
+                add_directory(&mut zip, &path, &root)?;
             }
         }
         Ok(())
@@ -37,6 +86,7 @@ pub fn compress_to_file(
     Ok(())
 }
 
+/// Decompress a zip file to their original path
 pub fn decompress_from_file(
     save_paths: Vec<SaveUnit>,
     backup_path: &PathBuf,
