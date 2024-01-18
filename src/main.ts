63d6fe9a78e581b24857d9e4ec121a4cd87eecbb
AsterNighT <asternight@outlook.com>
2024-01-18 16:40:59 +0800

    Feat: Add dark theme

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import ElementPlus from "element-plus";
+import 'element-plus/theme-chalk/dark/css-vars.css'
 import "element-plus/dist/index.css";
 import { createPinia } from 'pinia'
 
