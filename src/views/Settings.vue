394768a4dadb8eb586734d90bed3e69bb5fb53f4
AsterNighT <asternight@outlook.com>
2024-01-18 16:40:59 +0800

    Feat: Add dark theme

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -4,8 +4,11 @@ import { useConfig } from "../stores/ConfigFile";
 import { invoke } from "@tauri-apps/api/tauri";
 import { show_success } from "../utils/notifications";
 import { Game } from "../schemas/saveTypes";
+import { useDark, useToggle } from '@vueuse/core'
 
-
+const isDark = useDark()
+let isDarkDisplay = ref(isDark)
+const toggleDark = useToggle(isDark)
 const config = useConfig()
 const loading = ref(false)
 
@@ -45,8 +48,8 @@ function move_up(game: Game) {
 }
 function move_down(game: Game) {
     let index = config.games.findIndex((x) => x.name == game.name)
-    if(index!=config.games.length-1){
-        config.games[index] = config.games.splice(index+1, 1, config.games[index])[0];
+    if (index != config.games.length - 1) {
+        config.games[index] = config.games.splice(index + 1, 1, config.games[index])[0];
     }
 }
 
@@ -71,6 +74,10 @@ function move_down(game: Game) {
             <div class="setting-box">
                 <ElSwitch v-model="config.settings.extra_backup_when_apply" :loading="loading" />
                 <span>在应用存档时进行额外备份（在 ./save_data/游戏名/extra_backup 文件夹内）</span>
+            </div>
+            <div class="setting-box">
+                <ElSwitch :value="isDarkDisplay" :loading="loading" @change="toggleDark()" />
+                <span>启用夜间模式</span>
             </div>
             <div class="setting-box">
                 <ElCollapse>
