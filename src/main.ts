d0d9dd076218a425545db3a36831e204d5a6a13e
AsterNighT <asternight@outlook.com>
2024-01-19 16:34:01 +0800

    Feat: Add i18n, for settings only, for now
    Signed-off-by: AsterNighT <asternight@outlook.com>

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
+import { createI18n } from 'vue-i18n'
 import App from "./App.vue";
 import router from "./router";
 import ElementPlus from "element-plus";
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import "element-plus/dist/index.css";
 import { createPinia } from 'pinia'
+import { i18n } from "./i18n";
 
-createApp(App).use(router).use(createPinia()).use(ElementPlus).mount("#app");
+createApp(App).use(router).use(createPinia()).use(ElementPlus).use(i18n).mount("#app");
