dd88c0600b4b60a20ae892decd0d71a928ad711b
Sworld <61224072+mcthesw@users.noreply.github.com>
2025-10-08 22:44:51 +0800

    refactor(frontend): remove defineProps import from DeviceSetupDialog

--- src/components/DeviceSetupDialog.vue
+++ src/components/DeviceSetupDialog.vue
@@ -36,7 +36,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, defineProps, defineEmits, watch } from 'vue';
+import { ref, defineEmits, watch } from 'vue';
 import { $t } from '../i18n';
 import type { Device } from '../bindings';
 
